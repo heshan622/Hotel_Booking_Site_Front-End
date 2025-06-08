257d319bfc45242d9cf18e6aa1d7f28e8a0f0186
Pasindu Heshan <p.heshan622@gmail.com>
2025-06-08 20:15:42 +0530

    ExclusiveOfferc finalized

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
 import FeaturedDestination from '../components/FeaturedDestination.jsx'
+import ExclusiveOffers from '../components/ExclusiveOffers.jsx'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
     </>
   )
 }
