ae95f1bcebc3cd5d5def45682b8c00ab8002305b
Pasindu Heshan <p.heshan622@gmail.com>
2025-06-08 18:36:31 +0530

    FeaturedDestination page finalized

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
+import FeaturedDestination from '../components/FeaturedDestination.jsx'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
     </>
   )
 }
