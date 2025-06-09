40b56bc083443a76eac02b14195277e59fbe2c61
Pasindu Heshan <p.heshan622@gmail.com>
2025-06-09 15:05:06 +0530

    completed AllRoms page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import  Navbar from './components/Navbar.jsx'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Footer from './components/Footer.jsx'
+import AllRooms from './pages/AllRooms.jsx'
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/rooms' element={<AllRooms/>} />
       </Routes>
       </div>
       <Footer />
