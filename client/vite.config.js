64277f7326c5a3ed0275fde30a832582503c6603
Pasindu Heshan <p.heshan622@gmail.com>
2025-06-19 13:11:38 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : process.env.VITE_BASE_PATH || "/Hotel_Booking_Site_Frond-End",
 
   plugins: [    
     react(),
